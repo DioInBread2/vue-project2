2a2887f8abaf3c6fabc4ead34dba384b22060b68
DioInBread <1348402900@qq.com>
2025-03-19 23:13:16 +0800

    对话框 、表单

--- src/views/ElementDemo.vue
+++ src/views/ElementDemo.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import { ref , reactive } from 'vue';
 const tableData = [
   {date: '2016-05-03',name: 'Tom1',address: 'No. 189, Grove St, Los Angeles',},
   {date: '2016-05-02',name: 'Tom2',address: 'No. 189, Grove St, Los Angeles',},
@@ -15,6 +15,18 @@ const handleSizeChange = (val) => {
 }
 const handleCurrentChange = (val) => {
   console.log(`当前页码:${val}`)
+}
+
+const dialogTableVisible = ref(false)
+//表单
+const user = ref({
+  name: '',
+  gender: '',
+  birthday: '',
+})
+
+const onSubmit = () => {
+  console.log(user.value)
 }
 </script>
 
@@ -61,11 +73,57 @@ const handleCurrentChange = (val) => {
     />
    </div>
   
+   <!-- 对话框 -->
+   <div class="mb-4">
+    <el-button plain @click="dialogTableVisible = true"> 打开Dialog对话框</el-button>
 
+    <el-dialog v-model="dialogTableVisible" title="收货地址" width="800">
+    <el-table :data="tableData">
+      <el-table-column property="date" label="日期" width="150" />
+      <el-table-column property="name" label="姓名" width="200" />
+      <el-table-column property="address" label="地址" />
+    </el-table>
+  </el-dialog>
+   </div>
+
+<!-- 表单组件 -->
+ <div class="mb-4">
+    <el-form :inline="true" :model="user" class="demo-form-inline">
+    <el-form-item label="姓名">
+      <el-input v-model="user.name" placeholder="请输入姓名" clearable />
+    </el-form-item>
+    <el-form-item label="性别">
+      <el-select v-model="user.gender" placeholder="请选择" clearable>
+        <el-option label="男" value="1" />
+        <el-option label="女" value="2" />
+      </el-select>
+    </el-form-item>
+    <el-form-item label="生日">
+      <el-date-picker
+        v-model="user.birthday"
+        type="date"
+        placeholder="请选择"
+        value-format="YYYY-MM-DD"
+        clearable
+      />
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="onSubmit">Query</el-button>
+    </el-form-item>
+  </el-form>
+ </div>
 </template>
 
 <style scoped>
   .mb-4 {
     margin-bottom: 10px;
   }
+
+  .demo-form-inline .el-input {
+  --el-input-width: 220px;
+}
+
+.demo-form-inline .el-select {
+  --el-select-width: 220px;
+}
 </style>
